3d75f23349249df7d8bf406e4ea75392cff43173
Srilikhith Sajja <sajja.srilikhith@gmail.com>
2020-08-12 12:34:41 +0530

    final integration

--- maincalc.py
+++ maincalc.py
@@ -1,14 +1,16 @@
-#import add 
-#import subtract
-#import multiply
-#import divide
+import add 
+import subtract
+import multiply
+import divide
 import sys
 
 if sys.argv[2] == "+":
-    add.add(sys.argv[1],sys.argv[3])
+    result = add.add(sys.argv[1],sys.argv[3])
 elif sys.argv[2] == "-":
-    subtract.subtract(sys.argv[1],sys.argv[3])
+    result = subtract.subtract(sys.argv[1],sys.argv[3])
 elif sys.argv[2] == "*":
-    multiply.multiply(sys.argv[1],sys.argv[3])
+    result = multiply.multiply(sys.argv[1],sys.argv[3])
 elif sys.argv[2] == "/":
-    divide.divide(sys.argv[1],sys.argv[3])
+    result = divide.divide(sys.argv[1],sys.argv[3])
+
+print(result)
